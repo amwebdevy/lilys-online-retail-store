589a5882ec16f6f2d65f92b47ff4eca1a44c0f3b
amwebdevy <amwebdevy@gmail.com>
2019-07-03 23:39:41 -0400

    Removed unused requires from script

--- functions.php
+++ functions.php
@@ -3,20 +3,11 @@
 //ENQUEUE CSS/JS
 require get_template_directory() . '/inc/enqueue.php';
 
-//STRIP META WP VER. FROM FONT-END
-require get_template_directory() . '/inc/cleanup.php';
-
 //THEME SUPPORT FUNCTIONS
 require get_template_directory() . '/inc/theme-support.php';
 
-//CUSTOM ADMIN FUNCTIONS
-require get_template_directory() . '/inc/function-admin.php';
-
 //WALKER FUNCTIONS
 require get_template_directory() . '/inc/walker.php';
-
-//WIDGET FUNCTIONS
-require get_template_directory() . '/inc/widgets.php';
 
 //CUSTOM POST TYPE FUNCTIONS
 require get_template_directory() . '/inc/custom-post-type.php';
